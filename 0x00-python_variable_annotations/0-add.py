9c7f7a648462fbd995b10f3ef4e52f9edc5a02e3
Peter-Mwangi254 <pm6093628@gmail.com>
2024-06-06 19:46:33 +0000

    updates

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""
-Write a type-annotated function add that takes a float a and a float b
+"""Write a type-annotated function add that takes a float a and a float b
 as arguments and returns their sum as a float
 """
 
