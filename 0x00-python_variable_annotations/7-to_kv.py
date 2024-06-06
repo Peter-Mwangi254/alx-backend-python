48fdb4a5a8b6cafc4e3822244e5248a6fac8496e
Peter-Mwangi254 <pm6093628@gmail.com>
2024-06-06 21:50:51 +0000

    Complex types - string and int/float to tuple

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,1 +1,23 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""Write a type-annotated function to_kv that takes a string k
+and an int OR float v as arguments and returns a tuple.The first
+element of the tuple is the string k. The second element is the
+square of the int/float v and should be annotated as a float.
+"""
+
+import typing
+
+
+def to_kv(k: str, v: typing.Union[int, float]) -> typing.Tuple[str, float]:
+    """
+    Returns a tuple of the string k and the square of the value v as a float.
+
+    Args:
+        k (str): A string value.
+        v (Union[int, float]): An integer or float value.
+
+    Returns:
+        Tuple[str, float]: A tuple containing the string k
+        and the square of the value v as a float.
+    """
+    return (k, float(v * v))
