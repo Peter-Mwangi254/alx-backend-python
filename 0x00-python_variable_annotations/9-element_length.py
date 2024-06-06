cc74bb513839858c5a3954f6ddb3c8f23f5bb95a
Peter-Mwangi254 <pm6093628@gmail.com>
2024-06-06 22:10:48 +0000

    an iterable object

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,1 +1,33 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""Annotate the below function's parameters and return
+values with the appropriate types
+
+def element_length(lst):
+    return [(i, len(i)) for i in lst]
+
+{'lst': typing.Iterable[typing.Sequence], 'return': \
+    typing.List[typing.Tuple[typing.Sequence, int]]}
+"""
+
+
+import typing
+
+
+def element_length(lst: typing.Iterable[typing.Sequence]) -> \
+        typing.List[typing.Tuple[typing.Sequence, int]]:
+    """
+    Returns a list of tuples, where each tuple contains an
+    element from the input iterable
+    and the length of that element.
+
+    Args:
+        lst (Iterable[Sequence]): An iterable of sequences
+        (e.g., strings, lists, tuples).
+
+    Returns:
+        List[Tuple[Sequence, int]]: A list of tuples, where
+        the first element of each tuple is a sequence
+        from the input iterable and the second element is the
+        length of that sequence.
+    """
+    return [(i, len(i)) for i in lst]
